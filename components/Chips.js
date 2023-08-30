01a5bca66942153cf0e730d63f70a9064ab6c5e7
Shenelle <sdelapaz09@outlook.com>
2023-08-30 11:15:08 -0700

    first commit

---
+++ components/Chips.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import Counter from "./Counter";
+import '../style/Chips.css';
+
+const Chips = () => {
+    return (
+        <div>
+            <img className="chips"
+                src="https://media.giphy.com/media/3tY1Z1fM10AtPX6AwW/giphy.gif"
+                alt="Exploding bag of chips" />
+            <div className="link-block">
+                <h3>Spicy Jalapeno chips!</h3>
+                <h4>How many would you like?</h4>
+                <Counter />
+                <Link to="/"> Back to machine </Link>
+            </div>
+        </div>
+    );
+}
+
+export default Chips;
---
+++ components/Soda.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import Counter from "./Counter";
+import '../style/Soda.css';
+
+const Soda = () => {
+    return (
+        <div>
+            <img className="soda"
+                src="https://media.giphy.com/media/26BRNvGqt1cP1A5DG/giphy.gif"
+                alt="Pepsi is exploding everywhere" />
+            <div className="link-block">
+                <h3> This has way too much sugar! Stop drinking soda!</h3>
+                <h4> How many would you like?</h4>
+                <Counter />
+                <Link to="/">Back to MachineImage</Link>
+            </div>
+        </div>
+    );
+}
+
+export default Soda;
